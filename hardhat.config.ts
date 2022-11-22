d1b7c6013dcf4a91e8fa1c6ce06cb665711c7acc
0x1990c <mycluster1990@outlook.com>
2022-11-22 19:39:42 +0100

    fix: update hardhat config network setting with Goerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -46,8 +46,8 @@ const config: HardhatUserConfig = {
     ],
   },
   networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
